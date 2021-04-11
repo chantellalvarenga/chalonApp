2ffd5dc4c82ed0612dfcb92bf362dca97430066b
Josue Villeda <josuechacon46@gmail.com>
2021-04-11 17:25:50 -0600

    Cree metodos y validaciones para realizar una cita

--- chalonApp/app/src/main/java/com/example/chalonapp/activity_reserva.java
+++ chalonApp/app/src/main/java/com/example/chalonapp/activity_reserva.java
@@ -2,6 +2,8 @@ package com.example.chalonapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -52,47 +54,29 @@ public class activity_reserva extends AppCompatActivity implements View.OnClickL
         tvSetFecha = findViewById(R.id.tvSetFecha);
         tvSetHora = findViewById(R.id.tvSetHora);
 
-        //Obtenemos datos del usuario y del tratamiento seleccionado
-        Bundle bundle = getIntent().getExtras();
+          //Obtenemos datos del usuario y del tratamiento seleccionado
+          Bundle bundle = getIntent().getExtras();
 
           //Datos para realizar una reservación
-           id_cliente=bundle.getInt("id_cliente");
-           id_tratamiento=bundle.getInt("id_tratamiento");
-           nombre_cliente=bundle.getString("nombres_cliente");
-           apellidos_cliente=bundle.getString("apellidos_cliente");
-           nombre_tratamiento=bundle.getString("nombre_tratamiento");
-           precio_tratamiento=bundle.getDouble("precio_tratamiento");
-
-           /*String mensaje = "\n cliente: " + apellidos_cliente + "," + nombre_cliente +
-                            "\n Tratamiento: " + nombre_tratamiento +
-                            "\n Precio: " + String.valueOf(precio_tratamiento);
-           */
-           //textView.setText(mensaje);
+           id_cliente = bundle.getInt("id_cliente");
+           id_tratamiento = bundle.getInt("id_tratamiento");
+           nombre_cliente = bundle.getString("nombres_cliente");
+           apellidos_cliente = bundle.getString("apellidos_cliente");
+           nombre_tratamiento = bundle.getString("nombre_tratamiento");
+           precio_tratamiento = bundle.getDouble("precio_tratamiento");
 
            String CustomerName = "" + nombre_cliente + " " + apellidos_cliente + "";
-           tvActualUserName.setText(String.valueOf(id_cliente));
+           tvActualUserName.setText( String.valueOf(CustomerName) );
            tvTratamientoElegido.setText(nombre_tratamiento);
            tvPrecioTratamiento.setText("$ " + String.valueOf(precio_tratamiento));
 
-           /*
-        bfecha=(Button)findViewById(R.id.bfecha);
-        bhora=(Button)findViewById(R.id.bhora);
-        efecha=(EditText)findViewById(R.id.efecha);
-        ehora=(EditText)findViewById(R.id.ehora);
-        */
-
-        btnFechaLbl.setOnClickListener(this);
-        btnHoraLbl.setOnClickListener(this);
-
-
-    }
-
-    public void AgendarCita(View view){
-        Toast.makeText(activity_reserva.this,"Creando metodo para reservar", Toast.LENGTH_SHORT).show();
+           btnFechaLbl.setOnClickListener(this);
+           btnHoraLbl.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        //Registramos la fecha seleccionada en el TextView tvSetFecha
         if(v==btnFechaLbl){
             final Calendar c= Calendar.getInstance();
             dia=c.get(Calendar.DAY_OF_MONTH);
@@ -108,6 +92,8 @@ public class activity_reserva extends AppCompatActivity implements View.OnClickL
                     ,ano,mes,dia);
             datePickerDialog.show();
         }
+
+        //Registramos la hora en el TextView tvSetHora
         if (v==btnHoraLbl){
             final Calendar c= Calendar.getInstance();
             hora=c.get(Calendar.HOUR_OF_DAY);
@@ -121,5 +107,101 @@ public class activity_reserva extends AppCompatActivity implements View.OnClickL
             },hora,minutos,false);
             timePickerDialog.show();
         }
+    }
+
+    public void AgendarCita(View view){
+
+
+        String fechaAgendada = "";
+        fechaAgendada = tvSetFecha.getText().toString();
+
+        String horaAgendada = "";
+        horaAgendada = tvSetHora.getText().toString();
+
+        int hora = 0;
+        int minutos = 0;
+
+        //Primero validar que haya elegido fecha y hora
+        if ( !( fechaAgendada.equals("Elegir...") || horaAgendada.equals("Elegir...") || fechaAgendada.isEmpty() || horaAgendada.isEmpty() ) ){
+
+            String arrayHora[] = horaAgendada.split(":");
+            hora = Integer.parseInt( arrayHora[0] );
+            minutos = Integer.parseInt(arrayHora[1]);
+
+            if ( hora > 16 ){
+                Toast.makeText(activity_reserva.this,"Ops! Horario no disponible \nRegresa mañana...", Toast.LENGTH_SHORT).show();
+            }
+            else if( hora < 8 ){
+                Toast.makeText(activity_reserva.this,"Ops! Chalon abre a las 8 am...", Toast.LENGTH_SHORT).show();
+            }
+            else {
+
+                //Validar que ese usuario no tenga cita para ese mismo día y horario.
+
+                int idCitaExistente = 0;
+
+                //Conexión a la base de datos
+                SqlLiteOpenHelperAdmin admin = new SqlLiteOpenHelperAdmin(this,"chalon_database",null,1);
+                SQLiteDatabase database = admin.getReadableDatabase();
+
+                Cursor fila = database.rawQuery("select id from citas where id_cliente = '" + String.valueOf(id_cliente) + "' " +
+                        "AND fecha = '" + fechaAgendada + "' AND hora = '" + horaAgendada + "'",null);
+
+                if(fila.moveToFirst())
+                {
+                    idCitaExistente = fila.getInt(0);
+                }
+
+                if( idCitaExistente > 0 ){
+                    Toast.makeText(activity_reserva.this,"Ya tienes una cita programada para ese día y hora seleccionada", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    //Ahora verificamos que no tenga la cita para ese articulo para el mismo día
+                    int IdCitaMismodia = 0;
+
+                    Cursor fila2 = database.rawQuery("select id from citas where id_cliente = '" + String.valueOf(id_cliente) + "' " +
+                            "AND id_tratamiento = '" + String.valueOf(id_tratamiento) + "' " +
+                            "AND fecha = '" + fechaAgendada + "'",null);
+
+                    if(fila2.moveToFirst())
+                    {
+                        IdCitaMismodia = fila2.getInt(0);
+                    }
+
+                    if( IdCitaMismodia > 0 ){
+                        Toast.makeText(activity_reserva.this,"Ya tienes una cita programada para ese Tratamiento el mismo día", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+
+                        //Se agenda la cita
+
+                        //Generar ID
+
+                        int last_idCita = 0;
+                        Cursor filaID = database.rawQuery("SELECT MAX(id) FROM citas",null);
+
+                        if(filaID.moveToFirst())
+                        {
+                            last_idCita = filaID.getInt(0);
+                        }
+
+                        int New_idCita = last_idCita + 1;
+
+
+                        final String Insert_cita = "INSERT INTO citas VALUES( " + New_idCita + ", " + id_cliente + ", " + id_tratamiento + ", '1' , '" + fechaAgendada + "' , '" + horaAgendada + "')";
+                        database.execSQL(Insert_cita);
+
+                        Toast.makeText(activity_reserva.this,"¡Cita agendada exitosamente!", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+
+            }
+
+        }
+        else {
+            Toast.makeText(activity_reserva.this,"Elija una fecha y hora para su cita", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
